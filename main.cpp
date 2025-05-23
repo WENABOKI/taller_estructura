3759752700ecae37acb6b2a0a4d96222f1b8f72e
WENABOKI <manuel.morales@alumnos.ucn.cl>
2025-05-23 14:25:14 -0400

    aodjiweoij

--- main.cpp
+++ main.cpp
@@ -1,9 +1,21 @@
+#include <SFML/Graphics.hpp>
+#include <optional>
 
-#include <iostream>
+int main() {
+    sf::RenderWindow window(sf::VideoMode(800, 600), "Battleship");
 
-int main()
-{
-    std::cout<<"Hello World";
+    while (window.isOpen()) {
+        std::optional<sf::Event> event;
+        while ((event = window.pollEvent())) {
+            if (event->type == sf::Event::Closed) {
+                window.close();
+            }
+        }
+
+        window.clear();
+        // Aquí puedes dibujar cosas
+        window.display();
+    }
 
     return 0;
 }
